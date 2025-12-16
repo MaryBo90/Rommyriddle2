a24ea3536ef6f896f4a73bc314bb492730a9ed37
Mary <62451441+MaryBo90@users.noreply.github.com>
2025-12-16 23:59:10 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Rommyriddle2/', // <– diese Zeile hinzufügen
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
